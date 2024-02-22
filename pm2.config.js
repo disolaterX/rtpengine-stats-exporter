399d4acd38ad856f9c4cd82f8a58c961631ddf7c
Kartik Yadav <dev.kartik98@gmail.com>
2024-02-22 12:59:06 +0530

    <feat>: Add pm2.config.js file

---
+++ pm2.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: "rtpengine-stats-exporter", // Name of your application
+  script: "index.ts", // Entry point of your application
+  interpreter: "bun", // Path to the Bun interpreter
+};
